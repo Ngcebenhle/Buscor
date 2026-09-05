1340e72dff7c82d3df4dbdef3e048678a497b9a6
USER <ngcebenhleshabangu@gmail.com>
2026-09-05 20:08:41 +0200

    added code to switch pages on preference click on the ProfileandSettings Fragment

--- app/src/main/java/com/example/buscor/views/ProfileandSettings/ProfileandSettings.kt
+++ app/src/main/java/com/example/buscor/views/ProfileandSettings/ProfileandSettings.kt
@@ -5,14 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.buscor.R
+import com.example.buscor.views.ProfileandSettings.Cards.Cards
 
 class ProfileandSettings : PreferenceFragmentCompat()  {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+
+//        Page switch on Preference click
+
+
+        // 1. Find the preference item using its XML key
+        val accountPref: Preference? = findPreference("Loyalty_Points")
+
+        // 2. Set the click listener
+        accountPref?.setOnPreferenceClickListener {
+            // 3. Define the destination fragment
+            val newFragment = Cards()
+
+            // 4. Perform the fragment transaction to replace the current view
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, newFragment) // Replace with your container ID
+                .addToBackStack(null) // Allows the user to go back to Settings using the back button
+                .commit()
+
+            true // Return true to indicate the click was handled
+        }
+
+
     }
-
-
 }
