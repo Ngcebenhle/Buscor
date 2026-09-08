575bd8dab306949ea452931e5e8ce31a683a6f49
Ngceb'enhle <ngcebenhleshabangu@gmail.com>
2026-09-08 19:22:05 +0200

    Added skeleton UI for Schedule page.

---
+++ app/src/main/java/com/example/buscor/views/Schedules/Schedule.kt
@@ -0,0 +1,25 @@
+package com.example.buscor.views.Schedules
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.buscor.R
+
+class Schedule : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val View =  inflater.inflate(R.layout.fragment_schedule, container, false)
+
+//        Code Begins Here
+
+        return View
+    }
+
+
+}
