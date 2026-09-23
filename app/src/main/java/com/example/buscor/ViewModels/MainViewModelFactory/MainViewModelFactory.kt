dee04fadcfca246d4f7cd90b50f1d0e5005a8288
USER <ngcebenhleshabangu@gmail.com>
2026-09-23 22:01:23 +0200

    added viewmodel factory

--- app/src/main/java/com/example/buscor/ViewModels/MainViewModelFactory/MainViewModelFactory.kt
+++ app/src/main/java/com/example/buscor/ViewModels/MainViewModelFactory/MainViewModelFactory.kt
@@ -1,7 +1,17 @@
 package com.example.buscor.ViewModels.MainViewModelFactory
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.buscor.Model.MainRepository.MainRepository
 
 class MainViewModelFactory (private val repository: MainRepository) : ViewModelProvider.Factory {
 }
+
+//class MyViewModelFactory(private val myRepository: UserRepository) : ViewModelProvider.Factory {
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+//            return UserViewModel(myRepository) as T
+//        }
+//        throw IllegalArgumentException("Unknown ViewModel class")
+//    }
+//}
